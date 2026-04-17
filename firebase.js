3927d49fa1afeaeb23d139c94ff07915fae513e2
sarthakgarg7060 <129109819+sarthakgarg7060@users.noreply.github.com>
2026-04-17 17:38:31 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,18 +8,24 @@ import {
   signInWithPopup,
   signOut
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  collection,
+  getDocs,
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAROM1lMt_-VZSikZDNtl29xrAzbLwywgQ",
-  authDomain: "e--commerce-website-9b2fc.firebaseapp.com",
-  projectId: "e--commerce-website-9b2fc",
-  storageBucket: "e--commerce-website-9b2fc.firebasestorage.app",
-  messagingSenderId: "309334590038",
-  appId: "1:309334590038:web:22225776cf447d1c5571f7"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
 function signUpWithEmail(email, password) {
@@ -42,7 +48,23 @@ function watchAuthState(callback) {
   return onAuthStateChanged(auth, callback);
 }
 
+async function getAllProducts() {
+  const productsSnapshot = await getDocs(collection(db, "Products"));
+
+  return productsSnapshot.docs.map((doc) => {
+    const data = doc.data();
+
+    return {
+      id: doc.id,
+      name: data.name ?? "Untitled Product",
+      price: Number(data.price ?? 0),
+      image: data.image ?? ""
+    };
+  });
+}
+
 export {
+  getAllProducts,
   loginWithEmail,
   loginWithGoogle,
   logoutUser,
